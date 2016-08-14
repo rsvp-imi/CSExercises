d3367de3501197f02f99e1b93b592bb33ae2a804
rsvp-imi <rsvp.imi@gmail.com>
2016-08-14 14:03:15 +0800

    Completed Exercise 6

--- CSExercises/SectionB/ExB6.cs
+++ CSExercises/SectionB/ExB6.cs
@@ -16,13 +16,28 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
+           Console.WriteLine("Input x1, y1, x2, and y2");
+
+            string a1 = Console.ReadLine();
+            double x1 = Convert.ToDouble(a1);
+            string b1 = Console.ReadLine();
+            double y1 = Convert.ToDouble(b1);
+            string a2 = Console.ReadLine();
+            double x2 = Convert.ToDouble(a2);
+            string b2 = Console.ReadLine();
+            double y2 = Convert.ToDouble(b2);
+
+            double T = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+            Console.WriteLine("Distance Travelled is {0:0.00#}", Distance(T));
+
             //YOUR CODE HERE
         }
 
-        public static double CalculateDistance(double x1, double y1, double x2, double y2)
+        public static double Distance(double D)
         {
             //YOUR CODE HERE
-            return 0;
+            return (D);
         }
     }
 }
