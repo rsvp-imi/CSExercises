081e6b6a06e0169fdabebe3862e0af9205a8b5a0
rsvp-imi <rsvp.imi@gmail.com>
2016-08-17 13:08:46 +0800

    Updates

--- CSExercises/SectionE/ExE5.cs
+++ CSExercises/SectionE/ExE5.cs
@@ -15,7 +15,31 @@ namespace CSExercises
             //Hint: you can call ExE3.IsPrime method (from previous exercise) 
             //to check whether a number is a prime or not.
 
+            
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine (ExE3.IsPrime(n));
+
+            for (int n = 5; n<=1000; n++)
+
+                {
+                    int sum = 0;
+
+                for (int i = 2; i<n; i++)
+                {
+                   
+
+                    if (n % i == 0)
+                       sum = (sum + 1);
+                }
+                    if (sum == 0)
+
+                        Console.WriteLine (n);
+                }
+                
+
 
         }
+       
     }
 }
