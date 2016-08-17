96211f2e3c1efdfe94462c94ded735fe3b177a76
rsvp-imi <rsvp.imi@gmail.com>
2016-08-17 11:46:06 +0800

    Updates

--- CSExercises/SectionE/ExE4.cs
+++ CSExercises/SectionE/ExE4.cs
@@ -18,34 +18,49 @@ namespace CSExercises
             Console.Write("Please enter an integer: ");
             int n = Convert.ToInt32(Console.ReadLine());
 
-            if (IsPerfectNumber(n)) //number 'n' is passed here into 'IsPerfectNumber' function
+            if (IPN(n)) //number 'n' is passed here into 'IsPerfectNumber' function
             {
-                Console.WriteLine("Perfect Number");
+                Console.WriteLine("{0} is a Perfect Number", n);
             }
             else
             {
-                Console.WriteLine("Not Perfect Number");
+                Console.WriteLine("{0} is not a Perfect Number", n);
             }
         }
 
-        public static bool IsPerfectNumber(int n) //'n' gets transported here through the method // declaring 'bool' is vital to check true/false
+        public static bool IPN(int n) //'n' gets transported here through the method // declaring 'bool' is vital to check true/false
         {
-            int sum = 0;
+          //  int sum = 0;
             // initialized sum for this step, doesn't change as a result of the loop
           
 
-            for (int i = 1; i < n; i++ )
-                if (n % i == 0) //checks for values of 'i' that will divide 'n' and remaidner is '0'
+           // for (int i = 1; i < n; i++ )
+             //   if (n % i == 0) //checks for values of 'i' that will divide 'n' and remaidner is '0'
 
-                { sum = sum + i; } //sum changes with every perfect divide that gives remainder as '0'
+               // { sum = sum + i; } //sum changes with every perfect divide that gives remainder as '0'
             
-            if (sum == n) //to check if the number input is 'perfect'
+            //if (sum == n) //to check if the number input is 'perfect'
 
-            { return true; }
-            else
-            { return false; }          
+            //{ return true; }
+            //else
+            //{ return false; }          
 
+            bool IPN = true;
+
+            int sum = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                { sum = sum + i; }
+            }
+                if (sum != n)
+                { IPN = false; }
+
+                return IPN;
+            
+            }
 
         }
     }
-}
+
