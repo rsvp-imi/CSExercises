685ca50da622b12566b7775f1bfdf97694200036
rsvp-imi <rsvp.imi@gmail.com>
2016-08-17 20:43:50 +0800

    Updated

--- CSExercises/SectionF/ExF2.cs
+++ CSExercises/SectionF/ExF2.cs
@@ -13,21 +13,47 @@ namespace CSExercises
         {
             int[] list = new int[] {86, 35, 41, 49, 89, 51, 11, 71, 50, 75, 25, 58, 40, 14, 44, 3, 63, 81, 93, 79};
 
-            Sort(list);
+            /* Sort(list);
 
             for (int k = 0; k < list.Length; k++)
             {
                 Console.Write(list[k] + " ");
             }
-            Console.WriteLine();
+            Console.WriteLine();*/
+
+            for (int n = 0; n < list.Length; n++) // 'for-loop' used to display the numbers in a single row-list (**)
+            {
+                Console.Write(list[n] + " "); //Used to display the values in the array vai the 'for-loop' function. " " are used to create the spaces between the values
+            }
+            Console.WriteLine(); //used to get a line-break
+           
+            Sort(list);
+            
         }
 
-        public static void Sort(int[] values)
-        {
-            //YOUR CODE HERE
-            //Sort the list
+        public static void Sort(int[] list)
+        {                    
 
+            for (int i=0; i<list.Length-1; i++ )
+            {
+               for (int j=i+1; j<list.Length; j++)
+               {
+                   if (list[i] < list[j])
+                   {
+                       int Sort = list[j];
+                       list[j] = list[i];
+                       list[i] = Sort;
+                   }
+               }
+            }
+
+             for (int i=0; i<list.Length-1; i++ ) //instead of the "return" function, to create another loop, similar to the one in the main (**) to display the final list after sorting
+             {
+                 Console.Write(list[i] + " ");
+             }
+            Console.WriteLine();
+             }
         }
 
     }
-}
+
